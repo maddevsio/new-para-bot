dce911f460eea8cfafb229078862547b579bac66
puzanov <puzanov@gmail.com>
2018-03-21 01:17:04 +0600

    extracting pairs from binance

--- api/binance_test.go
+++ api/binance_test.go
@@ -3,14 +3,18 @@ package api
 import (
 	"testing"
 
+	"github.com/buger/jsonparser"
 	"gopkg.in/resty.v1"
 )
 
 func TestBinanceApi(t *testing.T) {
-	// just try to figure out how Binance API works
-	resp, err := resty.R().Get("https://api.binance.com/api/v1/LOOKING")
+	// iterate throught all active pairs
+	resp, err := resty.R().Get("https://api.binance.com/api/v1/exchangeInfo")
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
-	t.Logf("\nResponse Body: %v", resp)
+	jsonparser.ArrayEach([]byte(resp.String()), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		pair, _ := jsonparser.GetString(value, "symbol")
+		t.Log(pair)
+	}, "symbols")
 }
