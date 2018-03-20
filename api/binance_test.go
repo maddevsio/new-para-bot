df4475ca2936325e302f34bc25a5dbf348018f2c
puzanov <puzanov@gmail.com>
2018-03-20 22:41:30 +0600

    ready to make test api call

--- api/binance_test.go
+++ api/binance_test.go
@@ -3,9 +3,14 @@ package api
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"gopkg.in/resty.v1"
 )
 
 func TestBinanceApi(t *testing.T) {
-	assert.True(t, true)
+	// just try to figure out how Binance API works
+	resp, err := resty.R().Get("https://api.binance.com/api/v1/LOOKING")
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	t.Logf("\nResponse Body: %v", resp)
 }
