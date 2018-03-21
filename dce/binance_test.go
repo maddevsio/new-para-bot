c5fe7deaaf6d7afda3656cf4192978b6a4f1abee
puzanov <puzanov@gmail.com>
2018-03-21 22:49:03 +0600

    gorm integrated and tests added

--- dce/binance_test.go
+++ dce/binance_test.go
@@ -4,9 +4,16 @@ import (
 	"testing"
 
 	"github.com/buger/jsonparser"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/resty.v1"
 )
+
+type Binance struct {
+	gorm.Model
+	LastPairs string
+}
 
 func TestBinanceApi(t *testing.T) {
 	// iterate throught all active pairs
@@ -19,6 +26,23 @@ func TestBinanceApi(t *testing.T) {
 		pair, _ := jsonparser.GetString(value, "symbol")
 		pairs += pair + "\n"
 	}, "symbols")
+
+	// check popular pairs
 	assert.Contains(t, pairs, "ETHBTC")
 	assert.Contains(t, pairs, "XEMBNB")
+
+	// TODO: if no pairs in storage than do not alert! consider this as a first run
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&Binance{})
+	db.Create(&Binance{LastPairs: pairs})
+	var binance Binance
+	db.Last(&binance)
+	// this check is for GORM mainly
+	assert.Equal(t, pairs, binance.LastPairs)
+	db.Delete(&binance)
 }
