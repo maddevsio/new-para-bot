c229a1709c736b187006b6b50863b4c2ea61ec5a
puzanov <puzanov@gmail.com>
2018-03-25 10:11:12 +0600

    main logic was extracted to binance.go

--- dce/binance.go
+++ dce/binance.go
@@ -1,9 +1,98 @@
 package dce
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/buger/jsonparser"
+	"github.com/jinzhu/gorm"
+	resty "gopkg.in/resty.v1"
+)
 
 // Binance is a data struct for GORM to store prevours pairs from Binance
 type Binance struct {
 	gorm.Model
 	LastPairs string
+	URL       string `gorm:"-"`
+	DBPath    string `gorm:"-"`
+}
+
+// NewBinance is a Binance struct constructor
+func NewBinance() *Binance {
+	return &Binance{
+		URL:    "https://api.binance.com/api/v1/exchangeInfo",
+		DBPath: "/tmp/test.db", // TODO: need to handle this
+	}
+}
+
+// GetListOfActualPairs makes a call to API and returns \n separated pairs from api.binance.com
+func (b *Binance) GetListOfActualPairs() (string, error) {
+	// iterate throught all active pairs
+	resp, err := resty.R().Get(b.URL)
+	if err != nil {
+		return "", err
+	}
+	var pairs string
+	jsonparser.ArrayEach([]byte(resp.String()), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		pair, _ := jsonparser.GetString(value, "symbol")
+		pairs += pair + "\n"
+	}, "symbols")
+
+	return pairs, nil
+}
+
+// GetListOfSavedPairs returns the list of previously saved pairs, stored in sqlite
+func (b *Binance) GetListOfSavedPairs() (string, error) {
+	db, err := b.getDB()
+	defer db.Close()
+	if err != nil {
+		return "", err
+	}
+	db.Last(b)
+	return b.LastPairs, db.Error
+}
+
+// Count returns the number of all Binance records. This should be 0 or 1
+func (b *Binance) Count() (int, error) {
+	db, err := b.getDB()
+	defer db.Close()
+	if err != nil {
+		return 0, err
+	}
+	var count int
+	db.Model(&Binance{}).Count(&count)
+	return count, db.Error
+}
+
+// DeleteAll deletes all data from Binance table
+func (b *Binance) DeleteAll() error {
+	db, err := b.getDB()
+	defer db.Close()
+	if err != nil {
+		return err
+	}
+	db.DropTable(&Binance{})
+	return db.Error
+}
+
+// UpdatePairs deletes all data from Binance table and create new record with new pairs
+func (b *Binance) UpdatePairs(pairs string) error {
+	err := b.DeleteAll()
+	if err != nil {
+		return err
+	}
+	db, err := b.getDB()
+	defer db.Close()
+	if err != nil {
+		return err
+	}
+	db.Create(&Binance{LastPairs: pairs})
+	return db.Error
+}
+
+func (b *Binance) getDB() (*gorm.DB, error) {
+	// TODO: if no pairs in storage than do not alert! consider this as a first run
+	db, err := gorm.Open("sqlite3", b.DBPath)
+	if err != nil {
+		return nil, err
+	}
+	db.AutoMigrate(b)
+	return db, nil
 }
--- dce/binance_test.go
+++ dce/binance_test.go
@@ -4,52 +4,33 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/buger/jsonparser"
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/sergi/go-diff/diffmatchpatch"
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/resty.v1"
 )
 
 func TestBinanceApi(t *testing.T) {
-	// iterate throught all active pairs
-	resp, err := resty.R().Get("https://api.binance.com/api/v1/exchangeInfo")
-	if err != nil {
-		t.Errorf("Error: %v", err)
-	}
-	var pairs string
-	jsonparser.ArrayEach([]byte(resp.String()), func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-		pair, _ := jsonparser.GetString(value, "symbol")
-		pairs += pair + "\n"
-	}, "symbols")
-
-	// check popular pairs
-	assert.Contains(t, pairs, "ETHBTC")
-	assert.Contains(t, pairs, "XEMBNB")
-
-	// TODO: if no pairs in storage than do not alert! consider this as a first run
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
-
-	db.DropTable(&Binance{})
-	db.AutoMigrate(&Binance{})
-	var count int
-	db.Model(&Binance{}).Count(&count)
+	b := NewBinance()
+	actualPairs, err := b.GetListOfActualPairs()
+	assert.NoError(t, err)
+	assert.Contains(t, actualPairs, "ETHBTC") // check popular pairs
+	assert.Contains(t, actualPairs, "XEMBNB")
+	err = b.DeleteAll()
+	assert.NoError(t, err)
+	count, err := b.Count()
+	assert.NoError(t, err)
 	assert.Equal(t, 0, count)
-	db.Create(&Binance{LastPairs: pairs})
-	var binance Binance
-	db.Last(&binance)
+	err = b.UpdatePairs(actualPairs)
+	assert.NoError(t, err)
 	// this check is for GORM mainly
-	assert.Equal(t, pairs, binance.LastPairs)
+	savedPairs, err := b.GetListOfSavedPairs()
+	assert.NoError(t, err)
+	assert.Equal(t, actualPairs, savedPairs)
 
 	// add new pair and check diff with stored pairs
-	pairs += "KGZBTC\n"
+	actualPairs += "KGZBTC\n"
 	dmp := diffmatchpatch.New()
-	diffs := dmp.DiffMain(binance.LastPairs, pairs, true)
+	diffs := dmp.DiffMain(savedPairs, actualPairs, true)
 	var buff bytes.Buffer
 	for _, diff := range diffs {
 		text := diff.Text
@@ -61,5 +42,4 @@ func TestBinanceApi(t *testing.T) {
 		}
 	}
 	assert.Equal(t, "ADDED: KGZBTC\n", buff.String())
-	db.Model(&binance).Update("LastPair", pairs)
 }
